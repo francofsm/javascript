cc9c403a3ad4b8027e4c4fa7072ff4fd1d10cd9f
francofsm <31851872+francofsm@users.noreply.github.com>
2017-09-13 10:52:18 -0300

    VOL 3

---
+++ main.js
@@ -0,0 +1,112 @@
+
+/*
+var cafeTipo = "expreso";
+var cafeTemperatura = "caliente";
+var cafeOnzas = 100;
+var cafeLeche = true;
+
+var miCafe = {
+	tipo:"expreso",
+	temperatura: "caliente",
+	onzas: 100,
+	leche: true
+};
+
+alert(miCafe.onzas);
+
+---------------------------------
+
+var miCafe = {
+	tipo: "expreso",
+	temperatura: "caliente",
+	onzas: 100,
+	leche: true,
+
+	recalentar: function(){
+		if(miCafe.temperatura === "frio"){
+			miCafe.temperatura = "caliente";
+			alert("cafe recalentado");
+		}
+	}
+};
+
+miCafe.temperatura = "frio";
+miCafe.recalentar();
+
+--------------------------------------------
+
+var mark = {
+	name: "Mark",
+	poleraColor: "azul"
+};
+
+
+var lisa = {
+	name: "Lisa",
+	poleraColor: "rojo"
+};
+
+function Amigo(name, poleraColor){
+	this.name = name;
+	this.poleraColor = poleraColor;
+}
+
+var dani = new Amigo("Dani", "verde");
+
+alert(dani.name);
+
+-----------------------------------
+
+function hola(){
+	alert("hola");
+}
+
+window.hola();
+
+--------------------------
+var ejemplo = 2;
+
+var ejemplo = "ejemplo";
+
+alert(ejemplo);
+
+--------------------*/
+
+
+
+function alertNumber(){
+var uno = "1";
+alert(uno);
+
+function alertNumber2(){
+var dos = "2";
+alert(dos);
+
+}
+
+alertNumber2();
+}
+alertNumber();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
